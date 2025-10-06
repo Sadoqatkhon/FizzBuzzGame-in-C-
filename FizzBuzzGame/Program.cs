5efdad10fec0f85c8a4606225a074c909d5e5364
Sadokatkhon <sadoqatxonabdullayeva@gmail.com>
2025-10-06 19:39:49 +0500

    new feature: playing again with do/while

--- FizzBuzzGame/Program.cs
+++ FizzBuzzGame/Program.cs
@@ -10,46 +10,54 @@ namespace FizzBuzzGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's start our FizzBuzz Game!");
-            Console.Write("Enter a number limit for the game: ");
-            int gameStop = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Great");
-            Console.Write("What is our Fizz: ");
-            int fizz = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine();
-            Console.Write("What is our Buzz: ");
-            int buzz = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Awesome. Let's start");
-
-            for (int i = 1; i <= gameStop; i++)
+            int playAgain = 0;
+            do
             {
-                bool fizzDiv = i % fizz == 0;
-                bool buzzDiv = i % buzz == 0;
+                Console.WriteLine("Let's start our FizzBuzz Game!");
+                Console.Write("Enter a number limit for the game: ");
+                int gameStop = Convert.ToInt32(Console.ReadLine());
 
-                if (fizzDiv && buzzDiv)
+                Console.WriteLine("Great");
+                Console.Write("What is our Fizz: ");
+                int fizz = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.Write("What is our Buzz: ");
+                int buzz = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Awesome. Let's start");
+
+                for (int i = 1; i <= gameStop; i++)
                 {
-                    Console.WriteLine("FizzBuzz");
+                    bool fizzDiv = i % fizz == 0;
+                    bool buzzDiv = i % buzz == 0;
+
+                    if (fizzDiv && buzzDiv)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (fizzDiv)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (buzzDiv)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
                 }
-                else if (fizzDiv)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (buzzDiv)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }                
-            }
-            
-            Console.WriteLine();
-            Console.WriteLine("Here we go.Game is over");
-            Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("Here we go.Game is over");
+                Console.Write("Do you want to play again (1 for 'yes', 0 for 'no'): ");
+                playAgain = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+
+            } while (playAgain == 1);
 
         }
     }
